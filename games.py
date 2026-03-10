2ba25686be1ef3a604a1e9a2e26648615559f45c
jt691 <jt691@kent.ac.uk>
2026-03-10 21:41:01 +0000

    list down the basic games,score and exit feature in a def

--- games.py
+++ games.py
@@ -7,3 +7,11 @@ elif len (sys.argv) > 2:
     print("please enter ONLY your first name ")               #using argument to ask username 
 else:
     print("Welcome", sys.argv[1])
+
+def minigames():
+    print("Select a game you wish to play:")
+    print("1. Head or Tails")
+    print("2. Rock, Paper, Scissors")
+    print("3. Blackjack")
+    print("4. Score")
+    print("5. Exit")
