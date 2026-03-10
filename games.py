e5f6064ea8b8a123fbd974aa4f7335b01d4a9fe6
jt691 <jt691@kent.ac.uk>
2026-03-10 21:24:10 +0000

    username(arguments)

---
+++ games.py
@@ -0,0 +1,9 @@
+import sys                                                  #allows python to read the arguments #python3 games.py jack (line to input into the terminal)
+
+
+if len(sys.argv) < 2:
+    print("Please enter your first name ")
+elif len (sys.argv) > 2:
+    print("please enter ONLY your first name ")               #using argument to ask username 
+else:
+    print("Welcome", sys.argv[1])
